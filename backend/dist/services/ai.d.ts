63fe5bce9b51a8c8898d4af9c5e2d197bc2d1ac7
jenishk20 <kotharijenish2001@gmail.com>
2025-09-28 13:37:10 -0400

    fix: Remove dist files

--- backend/dist/services/ai.d.ts
+++
@@ -1,8 +0,0 @@
-import { Document } from "@langchain/core/documents";
-export declare const generateEmbedding: (text: string) => Promise<number[]>;
-export declare const generateSummaryForFile: (file: Document) => Promise<import("@langchain/core/messages").MessageContent>;
-export declare const generateSummaryForIssue: (issue: {
-    body: string;
-    title: string;
-}) => Promise<import("@langchain/core/messages").MessageContent>;
-//# sourceMappingURL=ai.d.ts.map
